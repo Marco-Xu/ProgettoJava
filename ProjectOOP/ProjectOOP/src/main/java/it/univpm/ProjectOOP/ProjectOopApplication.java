73b91811a859f0d09455be731be450c77967399f
Davide Balducci <s1092413@studenti.univpm.it>
2020-12-29 14:45:31 +0100

    Creazione della repository ProjectOOP

---
+++ ProjectOOP/ProjectOOP/src/main/java/it/univpm/ProjectOOP/ProjectOopApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.ProjectOOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOopApplication.class, args);
+	}
+
+}
---
+++ ProjectOOP/ProjectOOP/src/test/java/it/univpm/ProjectOOP/ProjectOopApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.ProjectOOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
