be4b46ddba2e114191211413ca84d6f8958506ce
Marco Xu <s1092678@studenti.univpm.it>
2021-01-11 18:16:29 +0100

    effetuato primo test

--- ProjectOOP/ProjectOOP/src/test/java/it/univpm/ProjectOOP/ProjectOopApplicationTests.java
+++ ProjectOOP/ProjectOOP/src/test/java/it/univpm/ProjectOOP/ProjectOopApplicationTests.java
@@ -3,11 +3,20 @@ package it.univpm.ProjectOOP;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import it.univpm.ProjectOOP.Data.DataWeather;
+import it.univpm.ProjectOOP.Data.MyData;
+import it.univpm.ProjectOOP.OpenWeather.ApiGet;
+
 @SpringBootTest
 class ProjectOopApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ApiGet api = new ApiGet();
+    	DataWeather dw = new DataWeather();
+    	MyData a = new MyData();
+    	a = dw.Parse(api.Read("Ancona"), "Ancona");
+    	System.out.println(a);
 	}
 
 }
