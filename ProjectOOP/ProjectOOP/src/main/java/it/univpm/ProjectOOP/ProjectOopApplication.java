1dc28d038b7f294f3cd86f4dfbc3c3f839200b52
Davide Balducci <s1092413@studenti.univpm.it>
2021-01-15 16:45:17 +0100

    dati storico

--- ProjectOOP/ProjectOOP/src/main/java/it/univpm/ProjectOOP/ProjectOopApplication.java
+++ ProjectOOP/ProjectOOP/src/main/java/it/univpm/ProjectOOP/ProjectOopApplication.java
@@ -2,7 +2,10 @@ package it.univpm.ProjectOOP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
 @SpringBootApplication
 public class ProjectOopApplication {
 
