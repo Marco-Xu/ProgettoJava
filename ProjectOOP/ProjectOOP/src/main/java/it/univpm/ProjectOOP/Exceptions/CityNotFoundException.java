6c42cf5d6fbb925d6f9c6eb750ebe157a724693e
Davide Balducci <s1092413@studenti.univpm.it>
2021-01-17 16:55:24 +0100

    Commentate le eccezioni

--- ProjectOOP/ProjectOOP/src/main/java/it/univpm/ProjectOOP/Exceptions/CityNotFoundException.java
+++ ProjectOOP/ProjectOOP/src/main/java/it/univpm/ProjectOOP/Exceptions/CityNotFoundException.java
@@ -1,10 +1,12 @@
 package it.univpm.ProjectOOP.Exceptions;
 
+/**Rappresenta un'eccezione personalizzata di tipo Exception.
+ * 
+ * @author Davide Balducci
+ * @author Marco Xu
+ */
 public class CityNotFoundException extends Exception{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public CityNotFoundException() {
--- ProjectOOP/ProjectOOP/src/main/java/it/univpm/ProjectOOP/Exceptions/NotIntegerException.java
+++ ProjectOOP/ProjectOOP/src/main/java/it/univpm/ProjectOOP/Exceptions/NotIntegerException.java
@@ -1,10 +1,12 @@
 package it.univpm.ProjectOOP.Exceptions;
 
+/**Rappresenta un'eccezione personalizzata di tipo Exception.
+ * 
+ * @author Marco Xu
+ * @author Davide Balducci
+ */
 public class NotIntegerException extends Exception{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public NotIntegerException() {
