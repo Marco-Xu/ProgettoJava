aac90dfa962fc8aaba478c5f4ae0a0859cc24fd3
Marco Xu <s1092678@studenti.univpm.it>
2021-01-17 16:09:24 +0100

    modifica ordine dei parametri

--- ProjectOOP/ProjectOOP/src/main/java/it/univpm/ProjectOOP/Data/MetaData.java
+++ ProjectOOP/ProjectOOP/src/main/java/it/univpm/ProjectOOP/Data/MetaData.java
@@ -10,15 +10,19 @@ public class MetaData {
 	public Vector<InfData> getMetadata(){
 		metadata.add(new InfData("city", "Nome della città", "String"));
 		metadata.add(new InfData("description", "Informazioni riguardanti il meteo", "String"));
+		
 		metadata.add(new InfData("normalTemp", "Temperatura media", "Double"));
 		metadata.add(new InfData("maximalTemp", "Temperatura massima", "Double"));
 		metadata.add(new InfData("minimalTemp", "Temperatura minima", "Double"));
+		
 		metadata.add(new InfData("feelsLikeTemp", "Temperatura percepita", "Double"));
-		metadata.add(new InfData("date", "Data in formato unix", "Int"));
-		metadata.add(new InfData("varNormalTemp", "Varianza temperatura media", "Double"));
-		metadata.add(new InfData("varFeelsLikeTemp", "Varianza temperatura percepita", "Double"));
 		metadata.add(new InfData("maxFeelsLikeTemp", "Temperatura massima percepita", "Double"));
 		metadata.add(new InfData("minFeelsLikeTemp", "Temperatura minima percepita", "Double"));
+		
+		metadata.add(new InfData("varNormalTemp", "Varianza temperatura media", "Double"));
+		metadata.add(new InfData("varFeelsLikeTemp", "Varianza temperatura percepita", "Double"));
+		
+		metadata.add(new InfData("date", "Data in formato unix", "Int"));
 		metadata.add(new InfData("n", "Numero elementi nel periodo indicato", "Int"));
 		
 		return metadata;
